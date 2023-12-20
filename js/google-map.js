541f0b03fec65bbc1c817a8aeef8913d8a320811
divyambhavsar36 <142592988+divyambhavsar36@users.noreply.github.com>
2023-12-20 21:43:21 +0530

    Update google-map.js

--- js/google-map.js
+++ js/google-map.js
@@ -45,7 +45,7 @@ function init() {
     var addresses = ['India'];
 
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('https://maps.app.goo.gl/qUUisY4hcWJK2cXBA', null, function (data) {
+        $.getJSON('https://www.google.com/maps/place/Ganesh+Glory+11+Rd,+Gota,+Ahmedabad,+Gujarat+382481/@23.1139717,72.5399421,17z/data=!3m1!4b1!4m6!3m5!1s0x395e82dc425f98b1:0xd62b93163625c6cf!8m2!3d23.1139717!4d72.5399421!16s%2Fg%2F11l6rqx_dr?entry=ttu', null, function (data) {
             var p = data.results[0].geometry.location
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
